338910ee927a531110081f62cccd57596db7a1ec
Harry Nguyen <dnncntt@gmail.com>
2020-03-05 05:15:28 +0100

    fix path render location

--- patch-rn.js
+++ patch-rn.js
@@ -1,29 +1,30 @@
 #!/usr/bin/env node
 
-const { writeFile, readFile, readdir } = require('fs');
-const { promisify } = require('util');
-const path = require('path');
+const { writeFile, readFile, readdir } = require("fs")
+const { promisify } = require("util")
+const path = require("path")
 
-const folder = 'node_modules/react-native/Libraries/Renderer/oss/';
+const folder = "node_modules/react-native/Libraries/Renderer/implementations/"
 
 const pattern = new RegExp(
-  'invariant\\([\\s\\S]{0,20}' +
-  '(hostContext|type)\\.isInAParentText,[\\s\\S]{0,20}' +
-  '"Text strings must be rendered within a <Text> component\\."[\\s\\S]{0,20}' +
-  '\\)[;,]'
-);
+  "throw ReactError[\\s\\S]{0,80}Text strings must be rendered within a <Text> component[\\s\\S]{0,80}[^()]\\)[;,]",
+  "gm"
+)
 
-const patchFile = async (file) => {
-  const content = (await promisify(readFile)(file)).toString();
-  const patched = content.replace(pattern, '');
-
-  await promisify(writeFile)(file, patched);
-};
+const patchFile = async file => {
+  const content = (await promisify(readFile)(file)).toString()
+  const patched = content.replace(pattern, () => {
+    const fileRegEx = new RegExp("dev")
+    if (fileRegEx.test(file)) return ""
+    else return "{}"
+  })
+  await promisify(writeFile)(file, patched)
+}
 
 const patchAll = async () => {
-  const files = await promisify(readdir)(folder);
+  const files = await promisify(readdir)(folder)
 
-  await Promise.all(files.map(file => path.join(folder, file)).map(patchFile));
-};
+  await Promise.all(files.map(file => path.join(folder, file)).map(patchFile))
+}
 
-patchAll();
+patchAll()
